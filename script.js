5ec172f260a1835c378041dbd85aa72075111d26
giang-1 <giang06121102@gmail.com>
2023-11-09 21:05:15 +0700

    bai tap js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-bài 1
+//bài 1
 var toan = 7;
 var ly = 8;
 var hoa = 9;
 var a = (toan + ly + hoa)/3
 document.write(a)
 
-bài 2
+//bài 2
 var c = 0;
 var f = (9*c)/5 + 32
 document.write(f);
 
-bài 3
+//bài 3
 var r = 10
 var pi = 3.14
 var s = pi * (r**2)
 document.write('diện tích:', s);
 
-bài 4
+//bài 4
 var h = r * pi;
 document.write('chu vi:', h);
